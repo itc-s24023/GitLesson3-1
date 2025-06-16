508f2a48e039d2d6877eb54501af4abd0152fc11
比嘉　力斗 <s24023@std.it-college.ac.jp>
2025-06-16 01:29:35 +0000

    厳等価過演算子(===)を使用するように修正

--- sort.js
+++ sort.js
@@ -1,9 +1,6 @@
-var number = [19,3,81,1,24,21];
-a.  console.log(number);
-
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-       if (a == b) {
+       if (a === b) {
            return 0;
        }
        return a < b ? -1 : 1;
